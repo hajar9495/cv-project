fc71bac1d36031b9c8c18528adb4ac09333317db
hajar9495 <hajarmennai@grpgformation.onmicrosoft.com>
2024-05-17 00:14:16 +0200

    projet en mobile design

---
+++ src/html/script.js
@@ -0,0 +1,9 @@
+// pour afficher le menu burger en mobile
+function afficherMenuBurger() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
